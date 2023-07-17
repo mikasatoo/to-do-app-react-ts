ca281e0d64959a79755178ff5b53bf86678826ca
Mika Sato <mika99@live.com.au>
2023-07-17 17:17:22 +1000

    Create input form in AddTodo.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { TodoList, AddTodo } from './components'
-import { Toaster } from 'react-hot-toast'
+import { TodoList, AddTodo } from './components';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-5xl font-bold">Todo App Tutorial</h1>
+    <div>
+      <Toaster position="bottom-center" />
+      <AddTodo />
+      <TodoList />
     </div>
-  )
+  );
 }
 
 export default App
