469f1d7e0af1ea6eb575e7a0e17b5f01e3e7fc7e
Mika Sato <mika99@live.com.au>
2023-07-18 12:31:47 +1000

    Create TodoContext

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,19 +1,34 @@
-import React, { createContext, useState } from 'react'
-import { nanoid } from 'nanoid'
-import { useLocalStorage } from 'usehooks-ts'
+import React, { createContext, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { useLocalStorage } from 'usehooks-ts';
 
-export interface Todo {
-  id: string
-  text: string
-  status: 'undone' | 'completed'
+// Define an interface for the TodoContextProps
+interface TodoContextProps {
+  todos: string[];
+  addTodo: (text: string) => void;
 }
 
-export const TodoContext = createContext<undefined>(undefined)
+// Create the TodoContext
+export const TodoContext = createContext<TodoContextProps | undefined>(
+  undefined,
+);
 
+// Provide the context
 export const TodoProvider = (props: { children: React.ReactNode }) => {
+  const [todos, setTodos] = useState<string[]>([]);
+  
+  const addTodo = (text: string) => {
+    setTodos([...todos, text]);
+  }
+
+  // Create the value for the context
+  const value: TodoContextProps = {
+    todos,
+    addTodo,
+  }
+
+  // Provide the context value
   return (
-    <TodoContext.Provider value={undefined}>
-      {props.children}
-    </TodoContext.Provider>
-  )
+    <TodoContext.Provider value={value}>{props.children}</TodoContext.Provider>
+  );
 }
