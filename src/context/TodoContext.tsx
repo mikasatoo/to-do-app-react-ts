40fbafccfa7299373223dbbacf46cecb46295906
Mika Sato <mika99@live.com.au>
2023-07-18 17:51:04 +1000

    Add to do item functionality
    Delete, edit, update status

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -6,13 +6,16 @@ import { useLocalStorage } from 'usehooks-ts';
 interface TodoContextProps {
   todos: Todo[]
   addTodo: (text: string) => void
+  deleteTodo: (id: string) => void
+  editTodo: (id: string, text: string) => void
+  updateTodoStatus: (id: string) => void
 }
 
 // Add an interface for the to do items
 export interface Todo {
   id: string
   text: string
-  status: 'incomplete' | 'complete'
+  status: 'incomplete' | 'completed'
 }
 
 // Create the TodoContext
@@ -22,8 +25,8 @@ export const TodoContext = createContext<TodoContextProps | undefined>(
 
 // Provide the context
 export const TodoProvider = (props: { children: React.ReactNode }) => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  
+  const [todos, setTodos] = useLocalStorage<Todo[]>('todos', []);
+
   // addTodo functionality
   const addTodo = (text: string) => {
     const newTodo: Todo = {
@@ -35,10 +38,45 @@ export const TodoProvider = (props: { children: React.ReactNode }) => {
     setTodos([...todos, newTodo]);
   }
 
+  // deleteTodo functionality
+  const deleteTodo = (id: string) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
+  // editTodo functionality
+  const editTodo = (id: string, text: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, text }
+        }
+        return todo;
+      });
+    });
+  }
+
+  // updateTodoStatus functionality
+  const updateTodoStatus = (id: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            status: todo.status === 'incomplete' ? 'completed' : 'incomplete',
+          }
+        }
+        return todo;
+      });
+    });
+  }
+
   // Create the value for the context
   const value: TodoContextProps = {
     todos,
     addTodo,
+    deleteTodo,
+    editTodo,
+    updateTodoStatus,
   }
 
   // Provide the context value
