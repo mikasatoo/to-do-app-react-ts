4a4e4527765b208058558b8581b972aa66699f24
Mika Sato <mika99@live.com.au>
2023-07-18 16:09:48 +1000

    Create the TodoItem component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,34 @@
-import type { Todo } from '../context'
-import { useEffect, useRef, useState } from 'react'
-import { useTodo } from '../context'
-import { Input } from './Input'
-import { BsCheck2Square } from 'react-icons/bs'
-import { TbRefresh } from 'react-icons/tb'
-import { FaRegEdit } from 'react-icons/fa'
-import { RiDeleteBin7Line } from 'react-icons/ri'
-import { toast } from 'react-hot-toast'
-import cn from 'classnames'
-import { motion } from 'framer-motion'
+import type { Todo } from '../context';
+import { useEffect, useRef, useState } from 'react';
+import { useTodo } from '../context';
+import { Input } from './Input';
+import { BsCheck2Square } from 'react-icons/bs';
+import { TbRefresh } from 'react-icons/tb';
+import { FaRegEdit } from 'react-icons/fa';
+import { RiDeleteBin7Line } from 'react-icons/ri';
+import { toast } from 'react-hot-toast';
+import cn from 'classnames';
+import { motion } from 'framer-motion';
 
 export const TodoItem = (props: { todo: Todo }) => {
-  return <div>Todo Item</div>
+  const { todo } = props;
+
+  return (
+    <motion.li
+      layout
+      className={cn(
+        'p-5 rounded-xl bg-zinc-900',
+        todo.status === 'completed' && 'bg-opacity-50 text-zinc-500',
+      )}
+    >
+      <motion.span
+        layout
+        style={{
+          textDecoration: todo.status === 'completed' ? 'line-through' : 'none',
+        }}
+      >
+        {todo.text}
+      </motion.span>
+    </motion.li>
+  );
 }
